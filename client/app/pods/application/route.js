590e57cb8a25f8e0c82fe80cab69bfba411b00fc
Giovanni Collazo <hello@gcollazo.com>
2020-01-11 12:52:08 -0400

    Fix data url in prod

--- client/app/pods/application/route.js
+++ client/app/pods/application/route.js
@@ -2,7 +2,7 @@ import Route from "@ember/routing/route";
 
 export default class ApplicationRoute extends Route {
   async model() {
-    let res = await fetch("http://localhost:3000/data.json");
+    let res = await fetch("/data.json");
     return res.json();
   }
 }
